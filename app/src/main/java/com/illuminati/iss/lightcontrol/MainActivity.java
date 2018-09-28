c84af392af4b0e4dffbc563f85af2bbd5a26a857
CHIRA Victor <VChira@isoftwaresystems.com>
2018-09-28 13:37:23 +0300

    read light sensor

--- app/src/main/java/com/illuminati/iss/lightcontrol/MainActivity.java
+++ app/src/main/java/com/illuminati/iss/lightcontrol/MainActivity.java
@@ -1,13 +1,61 @@
 package com.illuminati.iss.lightcontrol;
 
+import android.app.Service;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    SensorManager sensorManager;
+    Sensor sensor;
+    TextView tvLux;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvLux = (TextView) findViewById(R.id.tvLux);
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+//        try{
+//            UDPClient.send();
+//        }
+//        catch (Exception e)
+//        {
+//            Toast.makeText(this,e.toString(),1000);
+//        }
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType()==Sensor.TYPE_LIGHT)
+        {
+            tvLux.setText(""+event.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
