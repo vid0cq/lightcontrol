f6cfdfcd2b6a5d38e1a042a99543ce646f2625bb
BARBU Alexandra <ABarbu@isoftwaresystems.com>
2018-09-28 17:37:43 +0300

    UI changes

--- app/src/main/java/com/illuminati/iss/lightcontrol/MainActivity.java
+++ app/src/main/java/com/illuminati/iss/lightcontrol/MainActivity.java
@@ -2,12 +2,54 @@ package com.illuminati.iss.lightcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textview;
+    Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+
+        imageView = (ImageView)findViewById(R.id.imageView4);
     }
+
+    SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            // updated continuously as the user slides the thumb
+            if ( progress < 40)
+                imageView.setImageResource(R.drawable.sun1);
+            if ( progress >= 40)
+                imageView.setImageResource(R.drawable.sun2);
+            if ( progress >= 100)
+                imageView.setImageResource(R.drawable.sun2);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // called when the user first touches the SeekBar
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // called after the user finishes moving the SeekBar
+        }
+    };
 }
