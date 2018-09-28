7001f602b7f88523317747293641b553259ca339
BRUSTUREANU Bogdan <bbrustureanu@isoftwaresystems.com>
2018-09-28 09:04:09 +0300

    Test first commit

--- app/src/main/java/com/illuminati/iss/lightcontrol/NetworkExplorer.java
+++ app/src/main/java/com/illuminati/iss/lightcontrol/NetworkExplorer.java
@@ -1,4 +1,51 @@
 package com.illuminati.iss.lightcontrol;
 
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class NetworkExplorer {
+
+    public static HashMap<String, String> retardedScan()
+    {
+
+        int timeout=500;
+
+        HashMap<String, String> foundAddresses = new HashMap<>();
+
+        try
+        {
+            String currentIP = InetAddress.getLocalHost().toString();
+            String subnet = getSubnet(currentIP);
+
+            for (int i=1;i<254;i++){
+
+                String hostAddress = subnet + i;
+
+                InetAddress host = InetAddress.getByName(hostAddress);
+                if (host.isReachable(timeout))
+                {
+                    //address is reachable
+                    String hostName = host.getHostName();
+                    foundAddresses.put(hostAddress, hostName);
+                }
+            }
+        }
+        catch(Exception e){
+            //System.out.println(e);
+        }
+
+        return foundAddresses;
+    }
+
+    private static String getSubnet(String currentIP)
+    {
+        int firstSeparator = currentIP.lastIndexOf("/");
+        int lastSeparator = currentIP.lastIndexOf(".");
+        return currentIP.substring(firstSeparator+1, lastSeparator+1);
+    }
+
 }
